0c92463f09090217fd378ce674576a13de1d18ef
Matthew Smith <114437290+rm0819@users.noreply.github.com>
2024-06-26 13:43:54 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "HELLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOO";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="rm0819">
+  <meta name="description" content="rm0819's Various Web Projects">
+  <meta name="keywords" content="rm0819">
+  <title>rm0819's Homepage</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body class="home-content">
+    <?php include 'header.php'; ?>  
+
+</body> 
